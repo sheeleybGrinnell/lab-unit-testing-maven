5e07dd6ec3bd8e8abeca48097632c97e82e28f4b
Samuel A. Rebelsky <rebelsky@grinnell.edu>
2024-09-11 18:58:36 -0500

    Rename tests for random surefire.

--- src/test/java/edu/grinnell/csc207/util/TestSampleMethods.java
+++ src/test/java/edu/grinnell/csc207/util/TestSampleMethods.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
  * @author Samuel A. Rebelsky
  * @author Your Name Here
  */
-public class SampleMethodsTests {
+public class TestSampleMethods {
   /**
    * A test that should succeed.
    */
